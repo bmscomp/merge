721bf2bd2d1e130250b1e86059f1d2c5e49855e9
Said BOUDJELDA <bmscomp@gmail.com>
2025-01-03 01:04:51 +0100

    adjust the partial applicatino

--- src/main/java/com/bmscomp/partial/PartialApplication.java
+++ src/main/java/com/bmscomp/partial/PartialApplication.java
@@ -5,12 +5,17 @@ import java.util.function.Function;
 
 public record PartialApplication() {
 
-    Function<String, Integer> partialApplication(BiFunction<String, String, Integer> bi, String word) {
-        return s -> bi.apply(word, s);
+    private static BiFunction<String, String, Integer> bi = String::indexOf;
+
+    static Function<String, Integer> partialApplication(BiFunction<String, String, Integer> biFunction,
+                                                        String word) {
+        return s -> biFunction.apply(word, s);
     }
 
 
     public static void main(String[] args) {
         System.out.println("This is a demo for partial applications!");
+        var f = partialApplication(bi, "Hello");
+        System.out.println(f.apply("Hello"));
     }
 }
